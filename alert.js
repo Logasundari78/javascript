d702aa46617acab72fe1f86c98ca0c94c68ec6a0
Logasundari78 <alogu737353@gmail.com>
2025-06-23 09:19:45 +0530

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,15 @@
+//alert("welcome to this");
+console.log("welcome");
+console.log(124);
+console.log(124.98);
+console.log([54,98,78,88]);
+console.log({fname:'joes',age:25});
+console.table({fname:'joes',age:25});
+console.error("sample error");
+console.warn("wrong");
+console.clear();
+console.time("timer");
+for(i=0;i<10;i++){
+    console.log(i);
+}
+console.timeEnd("timer"); 
